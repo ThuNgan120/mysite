4a846c97bb95d81a4918a8c8dd091a6da9ae6545
Thu Ngân <nguyenthithungan8520@gmail.com>
2025-01-08 16:13:42 +0700

    second change

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  resources :products
+  root "products#index"
+
+
+  resource :session
+  resources :passwords, param: :token
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
 end
